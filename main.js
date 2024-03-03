be575ed18b61713b6e8e3ea15242b40e87ae69d7
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-03-03 10:27:13 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Question no 2
+//  let personName: string = "Ashrib"
+// console.log(`Helo ${personName} would you like to learn some typescript today?`)
+// Question no 3
+var Name = "Arish";
+//IN LOWER CASE
+console.log(Name.toLowerCase());
+// in upper case
+console.log(Name.toUpperCase());
+//in title case
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,16 @@
+ // Question no 2
+//  let personName: string = "Ashrib"
+// console.log(`Helo ${personName} would you like to learn some typescript today?`)
+
+
+// Question no 3
+let Name: string = "Arish"
+//IN LOWER CASE
+console.log(Name.toLowerCase());
+// in upper case
+console.log(Name.toUpperCase());
+//in title case
+console.log(Name.charAt(0).toUpperCase()+ Name.slice(1).toLowerCase());
+
+
+
